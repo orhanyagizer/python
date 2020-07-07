9ecbb6ac02dcbb03de40c1ada411773ff5a2f19b
Orhan <orhanyagizercinar@gmail.com>
2020-07-07 13:04:26 +0300

    Python Assignment-1 (Weekly Profit)

---
+++ assignment_1.py
@@ -0,0 +1,11 @@
+#Günlük %7 sabit kar
+money = 1000
+money +=  (money/100)*7
+money +=  (money/100)*7
+money +=  (money/100)*7
+money +=  (money/100)*7
+money +=  (money/100)*7
+money +=  (money/100)*7
+money +=  (money/100)*7
+
+print(int(money))  # int, ondalık kısmını göstermesin diye
