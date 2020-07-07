a3cf50fa48cc0d06b3bc07f01da99c5df7d40dd0
Orhan <orhanyagizercinar@gmail.com>
2020-07-07 13:00:24 +0300

    Python Assignment-3 (Measure Conventer)

---
+++ assignment_3.py
@@ -0,0 +1,12 @@
+print("Convert Celcius to Fahrenheit")
+degree = float(input("Please enter the degree: " ))
+new_degree = (degree * 1.8)+32
+output = f"{degree} degrees celcius equals {new_degree} degrees fahrenheit. "
+print(output)
+print()
+
+print("Convert kilometers to miles")
+distance = float(input("Please enter distance: "))
+new_distance = distance * 0.621371192
+output = f" {distance} kilometres equals {new_distance} mile."
+print(output)
