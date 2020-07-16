cecb1c94b0f730f8095eef53611f4872711c3945
Orhan <orhanyagizercinar@gmail.com>
2020-07-17 01:10:52 +0300

    Python Code Challange FizzBuzz with loops

---
+++ FizzBuzz.py
@@ -0,0 +1,15 @@
+#Print numbers from 1 to 100 inclusively following these instructions
+
+for i in range(1,101):
+   if i % 3 == 0:
+      print("Fizz")
+   elif i % 5 == 0:
+      print("Buzz")
+   
+   elif i % 3 == 0 and i % 5 == 0:
+      print("FizzBuzz")
+   
+   else:
+      print(i)   
+            
+      
