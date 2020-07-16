6b91cfebb5b4bd3201bed840c7f42eccac425bae
Orhan <orhanyagizercinar@gmail.com>
2020-07-17 01:14:32 +0300

    Python Code Challange FizzBuzz with for loops

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,4 +1,8 @@
 #Print numbers from 1 to 100 inclusively following these instructions
+#if a number is multiple of 3, print "Fizz" instead of this number,
+#if a number is multiple of 5, print "Buzz" instead of this number,
+#for numbers that are multiples of both 3 and 5, print "FizzBuzz",
+#print the rest of the numbers unchanged.
 
 for i in range(1,101):
    if i % 3 == 0:
